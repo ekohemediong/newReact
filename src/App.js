17b584202f5da49c8c19db9914187f3b17e15053
ekohemediong <108227171+ekohemediong@users.noreply.github.com>
2023-09-03 09:14:35 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React  ,{useState,useEffect,useRef,UseMemo} from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.css"
 export default function App() {
+  let [num,setNum]=useState ("")
+  let one=()
+  =>setNum((value)=>value+=1+"")
+  let two=()=>setNum((value)=>value+=2+"")
+  let three=()=>setNum((value)=>value+=3+"")
   return (
-    <div>
+    <div className='container p-2'>
+        <div>
+        <input type="text" value={num} className="form-control"
+        onChange={(e)=>setNum(e.target.value)} disabled placeholder="100"/>
+          
+        </div>
+      <div>
+        <button onClick={one}>
+          1</button>
+        <button onClick={two}>2</button>
+        <button onClick={three}>3</button>
+      </div>  
     </div>
   );
 }
